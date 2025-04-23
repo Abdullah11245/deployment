ed93c22a4617d16c85950f2c71f38fb49e143337
Abdullah11245 <abdullah.543711326@gmail.com>
2025-04-23 06:51:09 +0500

    Two seconds

--- src/app/RouteProtection.jsx
+++ src/app/RouteProtection.jsx
@@ -8,19 +8,23 @@ export default function ProtectedRoute({ children }) {
   const router = useRouter();
 
   useEffect(() => {
-    const user = localStorage.getItem('user');
+    const timeout = setTimeout(() => {
+      const user = localStorage.getItem('user');
 
-    if (!user) {
-      router.push('/Pages/Login');
-    } else {
-      setIsChecking(false); // User is found, stop checking
-    }
+      if (!user) {
+        router.push('/Pages/Login');
+      } else {
+        setIsChecking(false); // User is found, stop checking
+      }
+    }, 2000); // 2 seconds timeout
+
+    return () => clearTimeout(timeout); // Cleanup timeout on component unmount
   }, [router]);
 
   if (isChecking) {
     return (
       <div className="flex justify-center items-center h-screen">
-        <div className="flex justify-center items-center  h-screen w-screen flex-col fixed bg-white z-10">
+        <div className="flex justify-center items-center h-screen w-screen flex-col fixed bg-white z-10">
           <p className='text-lg mb-2'>Authenticating</p>
           <div className="flex space-x-2">
             <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:-0.3s]"></span>
