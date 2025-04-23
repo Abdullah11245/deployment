b5a7260ccaf657d30a5150d1844403f2cfa817c1
Abdullah11245 <abdullah.543711326@gmail.com>
2025-04-23 06:45:57 +0500

    Update

--- src/app/ClientLayoutwrapper.jsx
+++ src/app/ClientLayoutwrapper.jsx
@@ -8,30 +8,26 @@ import ProtectedRoute from './RouteProtection'; // adjust path
 export default function ClientLayoutWrapper({ children }) {
   const pathname = usePathname();
 
-  const noLayoutRoutes = ['/Pages/Login']; // etc.
+  // List of routes that don't need the full layout
+  const noLayoutRoutes = ['/Pages/Login']; 
   const isMinimalLayout = noLayoutRoutes.includes(pathname);
 
+  // If it's a route with no layout (like Login), we skip the layout
   if (isMinimalLayout) {
-    return (
-    <>
-    
-    {children}
-    </>
-    
-    );
+    return <>{children}</>; 
   }
 
   return (
-    <ProtectedRoute> {/* 🔒 Moved here */}
-        <Navbar />
-        <div className="flex">
-          <div className="mr-64">
-            <Sidebar />
-          </div>
-          <div className="w-full p-4 mt-16">
-            {children}
-          </div>
+    <ProtectedRoute> {/* 🔒 This will protect the layout */}
+      <Navbar />
+      <div className="flex">
+        <div className="mr-64">
+          <Sidebar />
         </div>
+        <div className="w-full p-4 mt-16">
+          {children} {/* Main content */}
+        </div>
+      </div>
     </ProtectedRoute>
   );
 }
