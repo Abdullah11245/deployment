f7b4caf1ed5d194bdcd5400f57a3481008773a3d
Abdullah11245 <abdullah.543711326@gmail.com>
2025-04-23 06:57:51 +0500

    Updated code

--- src/app/RouteProtection.jsx
+++ src/app/RouteProtection.jsx
@@ -23,15 +23,13 @@ export default function ProtectedRoute({ children }) {
 
   if (isChecking) {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="flex justify-center items-center h-screen w-screen flex-col fixed bg-white z-10">
-          <p className='text-lg mb-2'>Authenticating</p>
-          <div className="flex space-x-2">
-            <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:-0.3s]"></span>
-            <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:-0.15s]"></span>
-            <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce"></span>
-            <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:0.15s]"></span>
-          </div>
+      <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center h-screen w-screen flex-col bg-white z-50">
+        <p className="text-lg mb-2">Authenticating</p>
+        <div className="flex space-x-2">
+          <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:-0.3s]"></span>
+          <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:-0.15s]"></span>
+          <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce"></span>
+          <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:0.15s]"></span>
         </div>
       </div>
     );
